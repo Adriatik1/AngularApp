1ab0c6611fd99174a1e9d1ced0da6644aed6a66b
Adriatik Ademi <adriatik571@hotmail.com>
2020-03-18 10:16:40 +0100

    Extended API

---
+++ DatingApp.API/Data/Seed.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using System.Linq;
+using DatingApp.API.Models;
+using Newtonsoft.Json;
+
+namespace DatingApp.API.Data
+{
+    public class Seed
+    {
+        public static void SeedUsers(DataContext _context)
+        {
+            if(!_context.Users.Any())
+            {
+                var usersData = System.IO.File.ReadAllText("Data/UserSeedData.json");
+                var users = JsonConvert.DeserializeObject<List<User>>(usersData);
+                
+                foreach(var user in users)
+                {
+                    byte[] passwordHash, passwordSalt;
+                    CreatePasswordHash("password", out passwordHash, out passwordSalt);
+
+                    user.PasswordHash = passwordHash;
+                    user.PasswordSalt = passwordSalt;
+                    user.Username = user.Username.ToLower();
+                    _context.Users.Add(user);
+                }
+                _context.SaveChanges();
+            }
+        }
+
+        public static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using(var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+    }
+}
